e47c9ed40d2a09d42866b880cd9e8309b3605131
PythonsFavorite <159999223+PythonsFavorite@users.noreply.github.com>
2024-02-29 00:46:26 +0800

    Add files via upload

---
+++ S11.cpp
@@ -0,0 +1,22 @@
+// Циклически перебрать числа от 0 до 100. Если число делится на 3 вывести его на экран
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Через цикл while
+    setlocale(0, "Rus");
+    /*int a;
+    while (a !=100 ) {
+            a++;
+    if (a % 3 == 0) cout << a << endl; }
+*/
+//Через цикл for
+int a;
+  for (a; a != 100; a++)
+  if ((a % 3== 0) && (a != 0))  cout << a << endl;
+
+
+    return 0;
+}

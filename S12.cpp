7d50a5e2a175be887b1314c91aa5f0acef361405
PythonsFavorite <159999223+PythonsFavorite@users.noreply.github.com>
2024-02-29 00:47:45 +0800

    Add files via upload

---
+++ S12.cpp
@@ -0,0 +1,34 @@
+//Используя другой тип цикла, перебрать числа от 1 до 1000 с шагом 4. Найти сумму всех чисел,которые делятся на 7 без остатка
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, summa;
+    summa = 0;
+for (a=1; a <1000; a+= 4)
+if (a % 7 == 0) {
+    summa += a;
+    cout << summa << endl;
+}
+    //Через цикл while
+    /*int a, summa;
+    summa = 0;
+    while (a < 1000) {
+        a += 4;
+        if (a % 7 == 0) {
+            summa += a;
+            cout << summa << endl;
+        }
+    }
+*/
+
+
+
+
+
+
+
+    return 0;
+}

d5fac36e69898e23b16e01b41b13ba843fe5180a
PythonsFavorite <159999223+PythonsFavorite@users.noreply.github.com>
2024-03-25 19:28:44 +0800

    Add files via upload

---
+++ S41.cpp
@@ -0,0 +1,28 @@
+//S41 (1б) Напишите функцию squared_sum(), которая принимает два дробных числа и возвращает квадрат их суммы
+#include <iostream>
+
+using namespace std;
+
+float squared_sum(float a, float b)
+{
+    float summa = a + b;
+    float squred = summa * summa;
+    return squred;
+}
+
+
+int main()
+{
+    setlocale(0, "Rus");
+    float number1, number2;
+    cout << "Введите число 1: ";
+    cin >> number1;
+    cout << "Введите число 2: ";
+    cin >> number2;
+
+    cout << squared_sum(number1, number2)<< endl;
+
+
+
+    return 0;
+}

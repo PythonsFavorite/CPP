c39c9215abcc2b632fdbb0c55b98402b00dc5fc8
PythonsFavorite <159999223+PythonsFavorite@users.noreply.github.com>
2024-04-04 16:23:20 +0800

    Add files via upload

---
+++ M02.cpp
@@ -0,0 +1,29 @@
+//М02 (3б) Функция принимает два параметра: сторона А и число сторон N, и должна вернуть площадь N-угольника со стороной А.
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+float square (float A, int N)
+{
+    float const PI = 3.141593;
+    float grad = 1/tan(180/N * PI/180);  //Перевод ctg из радианов в грудусы
+    float S = (N*(A*A)/4) * grad;
+
+    return S;
+}
+
+
+
+int main()
+{
+    float side;
+    int kolvo_side;
+    setlocale(0, "Rus");
+    cout << "Введите сторону: ";
+    cin >> side;
+    cout << endl << "Введите число сторон: ";
+    cin >> kolvo_side;
+    cout << square(side,kolvo_side) << endl;
+
+    return 0;
+}
